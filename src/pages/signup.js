5c8072e4b0620e6cb271427c060ca5980583ff3d
Ankur Yadav <ankury.2508@gmail.com>
2023-03-24 16:43:40 +0530

    initial commit

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,9 +5,8 @@ import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const Home = () => {
+const signup = () => {
   const router = useRouter();
-  const [values, setValues] = useState({ username: "", password: "" });
   const toastOptions = {
     position: "bottom-right",
     autoClose: 8000,
@@ -16,17 +15,39 @@ const Home = () => {
     theme: "dark",
   };
 
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
-  const validateForm = () => {
-    const { username, password } = values;
-    if (username === "") {
-      toast.error("Email and Password is required.", toastOptions);
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
       return false;
-    } else if (password === "") {
-      toast.error("Email and Password is required.", toastOptions);
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
       return false;
     }
     return true;
@@ -34,8 +55,8 @@ const Home = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (validateForm()) {
-      router.push("/signup");
+    if (handleValidation()) {
+      router.push("/");
     }
   };
 
@@ -51,7 +72,12 @@ const Home = () => {
             placeholder="Username"
             name="username"
             onChange={(e) => handleChange(e)}
-            min="3"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
           />
           <input
             type="password"
@@ -59,9 +85,15 @@ const Home = () => {
             name="password"
             onChange={(e) => handleChange(e)}
           />
-          <button type="submit">Sign In</button>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Sign Up</button>
           <span>
-            Don't have an account ? <Link href="/signup">CREATE ONE</Link>
+            Already have an account ? <Link href="/">SIGN IN</Link>
           </span>
         </form>
       </FormContainer>
@@ -103,13 +135,13 @@ const FormContainer = styled.div`
   input {
     background-color: transparent;
     padding: 1rem;
-    border: 0.1rem solid #4e0eff;
-    border-radius: 0.4rem;
+    border: none;
+    border-bottom: 0.1rem solid #4e0eff;
     color: white;
     width: 100%;
     font-size: 1rem;
     &:focus {
-      border: 0.1rem solid #997af0;
+      border-bottom: 0.1rem solid #997af0;
       outline: none;
     }
   }
@@ -138,4 +170,4 @@ const FormContainer = styled.div`
   }
 `;
 
-export default Home;
+export default signup;
