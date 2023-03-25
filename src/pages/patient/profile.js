a167eb9a21672d1b43389abf415fa4f7bd7b006b
Arinaymay Bhaskar <arinaymay.bhaskar.7500@gmail.com>
2023-03-25 23:48:50 +0530

    early profile structure

--- src/pages/patient/profile.js
+++ src/pages/patient/profile.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
 
-const profile = () => {
-  return <div>profile</div>;
-};
+function Profile(props) {
+  
+  return (
+    <div className="profile-container">
+      <div className="profile-image">
+        <img src="/profile" alt="" />
+      </div>
+      <div className='profile-line'></div>
+      <div className="profile-details">
+        <div className="profile-name-age">
+          <div className='profile-name'>Name</div>
+          <div className='profile-age'>Age</div>
+        </div>
+        <div className="profile-email">
+          <p>Email: </p>
+        </div>
+        <div className="profile-contact">
+          <p>Contact: </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default profile;
+
+export default Profile;
+
