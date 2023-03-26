0e554d1b4ac341232c5f015e8a347f1717b14bfd
Arinaymay Bhaskar <arinaymay.bhaskar.7500@gmail.com>
2023-03-26 21:58:51 +0530

    profile html updated

--- src/pages/patient/profile.js
+++ src/pages/patient/profile.js
@@ -1,25 +1,66 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+import Navbar from "@/components/Navbar";
 
 function Profile(props) {
-  
+
   return (
+    <div>
+      <Navbar />
+
     <div className="profile-container">
       <div className="profile-image">
-        <img src="/profile" alt="" />
+        <div>
+          <h2 className='image-name'>Name</h2>
+        </div>
+        <div className='image-container'>
+          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+        </div>
+        <div>
+          <button className='Upload-photo-btn'>Upload New Photo</button>
+        </div>
       </div>
-      <div className='profile-line'></div>
       <div className="profile-details">
-        <div className="profile-name-age">
-          <div className='profile-name'>Name</div>
-          <div className='profile-age'>Age</div>
+        <div className='edit-profile'><h1>Edit Profile</h1> </div>
+        <div className='edit-profile-form'>
+          <div className="input">
+            <input type="text" className="input-field" required />
+            <label class="input-label">Full name</label>
+          </div>
+          <div className="input">
+            <input type="date" className="input-field-date" required />
+            <label class="input-label"><p>Date of birth</p></label>
+          </div>
+
+          <div className="input">
+            <input type="text" className="input-field" required />
+            <label className="input-label">Email</label>
+          </div>
+          <div className="input">
+            <input type="password" className="input-field" required />
+            <label className="input-label">Password</label>
+          </div>
+          <div className="input">
+            <input type="number" className="input-field" required />
+            <label className="input-label">Contact number</label>
+          </div>
+          <div className="input">
+            <label className="input-label">Gender</label>
+            <select className='input-field'>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Transgender">Transgender</option>
+              <option value="Rather not say">Rather not say</option>
+            </select>
+          </div>
         </div>
-        <div className="profile-email">
-          <p>Email: </p>
-        </div>
-        <div className="profile-contact">
-          <p>Contact: </p>
-        </div>
+        <div className='profile-detail-buttons'>
+            <button className='Upload-detail-btn'>Continue without saving</button>
+            <button className='Upload-detail-btn'>Update Profile</button>
+          </div>
       </div>
+
+    </div>
     </div>
   );
 }
